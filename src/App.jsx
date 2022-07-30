8047a11a211c4448f22c25bd745f646dcd505eb6
unknown <guilhermekhirata@hotmail.com>
2022-07-30 12:50:54 -0300

    feat/ criacao de novas funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
-import Header from './components/Header';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart';
-import { useEffect, useState } from 'react';
-import styled from './style.module.css'
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import { useEffect, useState } from "react";
+import styled from "./style.module.css";
 
 function App() {
-
-  const [products, setProducts] = useState([])
-  const [filteredProducts, setFilteredProducts] = useState([])
-  const [currentSale, setCurrenteSale] = useState([])
-  const [cartTotal, setCartTotal] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrenteSale] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     fetch("https://hamburgueria-kenzie-json-serve.herokuapp.com/products")
-    .then(res => res.json())
-    .then(res => setProducts(res))
-    .catch(err => console.log(err))
-  }, [])
-  
-  const showProducts = () => {
+      .then((res) => res.json())
+      .then((res) => setProducts(res))
+      .catch((err) => console.log(err));
+  }, []);
 
-  }
+  const showProducts = () => {
+    const productsFiltered = products.filter((product) => product.name.toLowerCase().includes(filteredProducts))
+    setProducts(productsFiltered)
+  };
 
   const handleClick = (productId) => {
-    const productFound = products.find((elem) => elem.id === productId)
-    setCurrenteSale([...currentSale, productFound])
-  }
+    const productFound = products.find((elem) => elem.id === productId);
+    const verification = currentSale.find((elem) => elem.id === productId);
+    if (verification === undefined) {
+      setCurrenteSale([...currentSale, productFound]);
+    }
+  };
 
   return (
     <>
-      <Header/>
+      <Header filteredProducts={filteredProducts} setFilteredProducts={setFilteredProducts} showProducts={showProducts}/>
       <div className={styled.mainContainer}>
-        <ProductList products={products} handleClick={handleClick}/>
-        <Cart currentSale={currentSale} cartTotal={cartTotal} setCartTotal={setCartTotal}/>
+        <ProductList products={products} handleClick={handleClick} />
+        <Cart
+          currentSale={currentSale}
+          setCurrenteSale={setCurrenteSale}
+          cartTotal={cartTotal}
+          setCartTotal={setCartTotal}
+        />
       </div>
     </>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,26 @@
-import styled from "./style.module.css"
+import styled from "./style.module.css";
 
-const Header = () => {
-    return (
-        <header>
-            <h1>Burguer <span>Kenzie</span></h1>
-            <div>
-                <input 
-                type="text" 
-                placeholder="Digitar Pesquisa"
-                />
-                <button type="submit">Pesquisar</button>
-            </div>
-        </header>
-    )
-}
+const Header = ({filteredProducts ,setFilteredProducts, showProducts}) => {
+  return (
+    <header>
+      <h1>
+        Burguer <span>Kenzie</span>
+      </h1>
+      <div>
+        <input 
+        type="text" 
+        placeholder="Digitar Pesquisa" 
+        value={filteredProducts}
+        onChange={(event) => setFilteredProducts(event.target.value.toLowerCase())}
+        />
+        <button 
+        type="submit"
+        onClick={() => showProducts()}
+        >Pesquisar</button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
+
